e70e8b950365e7198870da279f1d4a6d5f70e405
Efe Hascan <82836263+efehascan@users.noreply.github.com>
2024-11-12 16:43:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Luhn Algoritması");
+
+static bool CreditCard(string cardNumber)
+{
+    cardNumber = cardNumber.Replace(" ", "");
+    if (!cardNumber.All(char.IsDigit)) return false;
+
+    int sum = cardNumber
+        .Reverse()
+        .Select((c, i) =>
+        {
+            int digit = c - '0';
+            if (i % 2 == 1) digit = digit * 2 > 9 ? digit * 2 - 9 : digit * 2;
+            return digit;
+        })
+        .Sum();
+    return sum % 10 == 0;
+}
+
+string creditCardNumber = "4847 3529 8926 3094";
+bool isValid = CreditCard(creditCardNumber);
+Console.WriteLine(isValid);
